50366f915138a318487b20558b18b8d2f2e530eb
ierarkhovitch <ierarkhovitch@yandex.ru>
2020-07-29 12:02:50 +0300

    Добавили category для views с ограничением (only get and post), Прикрутили ursl для swagger.

--- backend/prj/prj/urls.py
+++ backend/prj/prj/urls.py
@@ -14,8 +14,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions, routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from market.views.category import CategoryViewSet
+
+router = routers.DefaultRouter()
+router.register(r'category', CategoryViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Delivery API",
+        default_version='v1',
+        description='''
+        Documentation `ReDoc` view can be found [here](/doc).
+        ''',
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('V1/', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('doc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
